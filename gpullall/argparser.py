04c6c27983f4f851cce14847506d455af96b86b3
jesudanielcf <jesusdanielcf@gmail.com>
2019-08-02 13:24:53 +0200

    Ignore directories

--- gpullall/argparser.py
+++ gpullall/argparser.py
@@ -1,12 +1,20 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Pull all your GIT repositories.')
-parser.add_argument('--directory', '-D',
-                    help="Enter a directory manually.", nargs=1)
-parser.add_argument('--confirmall', '-Y',
+parser.add_argument('--directory',
+                    '-D',
+                    help="Enter a directory manually.",
+                    nargs=1)
+parser.add_argument('--confirmall',
+                    '-Y',
                     help="Confirm to pull every single"
                          + "repository found automatically.",
                     action="store_true")
-parser.add_argument('--fullscan', '-F',
+parser.add_argument('--fullscan',
+                    '-F',
                     help="Search git repositories in the disk.",
                     action="store_true")
+parser.add_argument('--ignore',
+                    '-I',
+                    help="Ignore a folder(s). eg: --ignore dir1,dir2,dir3...",
+                    nargs=1)
